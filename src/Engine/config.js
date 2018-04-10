78e5367e56e4f6c9b6e92d7160949bd8e3eb341d
Roelof Kallenkoot <roelof000@gmail.com>
2018-04-10 11:38:25 +0100

    Added transparent image for bounding box, CS for Engine code, 64x16 map and level array

--- src/Engine/config.js
+++ src/Engine/config.js
@@ -18,30 +18,30 @@ Game = (function(module) {
     player_top_speed : 6,
 
     player_jump_speed : 8,
-    
+
     player_sprite_scale : 1,
 
     player_controls : { moveLeft : 'LEFT', moveRight : 'RIGHT', jump : 'SPACE' },
-    
-    
+
+
     pickup_sprite_scale : 0.75, // Bigger number = larger sprite!
 
     pickup_time_delay : 5, // Delay (in seconds) between pickups appearing
 
     frictionCoeff : 0.025,
-    
+
     default_creature_mass : 18,
     default_creature_sprite_scale : 1,
     default_creature_damage : 10,
-    
+
     default_platform_mass : 20,
     default_platform_sprite_scale : 1,
-    
+
     show_bounding_volume : true
   };
-  
-  
+
+
   return module;
-  
+
 })((window.Game || {}));
 
--- src/Engine/src/background.js
+++ src/Engine/src/background.js
@@ -4,24 +4,24 @@
 // Model background images with parallax scrolling
 
 Game = (function(module) {
-  
+
   module.Background = function(params) {
-  
+
     self = {}
-    
+
     // Private state
     let ps = {
-      
+
       n : (params.images.length || 0),
       sigma : (params.sigma || 2.0),
       images : (function() { console.log('foo'); return [];})(),
       scale : []
-      
+
       /*
-      // Fit background image to camera or allow it to 
+      // Fit background image to camera or allow it to
       // scroll
       mainBackground : {
-      
+
         // If fitToCamera is true, the main background remains
         // fixed wrt the viewport of the camera.  If true,
         // the aspect ratio can be maintained (where relevant
@@ -30,38 +30,38 @@ Game = (function(module) {
         fitToCamera : (params.fitToCamera || true),
         maintainAspect : (params.maintainAspect || true)
       },
-      
+
       intermediateBackground : {
-        
+
       }
       */
     }
-    
+
     // Populate private state
     for (let i=0; i<ps.n; i++) {
-      
+
       ps.images[i] = Game.Graphics.Sprite({ imageURL : params.images[i] })
-      
+
       if (i<ps.n - 1) {
-      
-        ps.scale[i] = Math.pow(ps.sigma, -i)      
+
+        ps.scale[i] = Math.pow(ps.sigma, -i)
       }
       else {
-        
+
       }
     }
-    
-    
+
+
     // Private API
-    
-    
+
+
     // Public Interface
-    
-    
+
+
   }
-  
+
   console.log('bar');
-  
+
   return module
-  
+
 })(window.Game || {})
